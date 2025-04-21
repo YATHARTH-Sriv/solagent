70188902dbcf0069f2c97aacf819be968a779611
yatharth srivastava <yatharthsriv18@gmail.com>
2025-04-21 06:21:06 +0530

    linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,7 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     "rules": {
-      " react-hooks/exhaustive-deps": "off"
+      "@react-hooks/exhaustive-deps": "off"
     }
   }
 ];
